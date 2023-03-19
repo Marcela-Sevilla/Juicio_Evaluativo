a0013fcb85201c507cfab966db67dafc6a595970
Marcela-Sevilla <sevillamarcela514@gmail.com>
2023-03-19 18:44:50 -0500

    Creando vista para la importación de los archivos

---
+++ app/Imports/ReporteImport.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Imports;
+
+use App\Models\Reporte;
+use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithBatchInserts;
+use Maatwebsite\Excel\Concerns\WithChunkReading;
+
+class ReporteImport implements ToModel
+{
+    /**
+    * @param array $row
+    *
+    * @return \Illuminate\Database\Eloquent\Model|null
+    */
+    public function model(array $row)
+    {
+        return new Reporte([
+            'tipo_documento' => $row['tipo_documento'],
+            'numero_documento' => $row['numero_documento'],
+            'nombre_completo' => $row['nombre'],
+            'estado' => $row['estado'],
+        ]);
+    }
+
+    public function batchSize(): int
+    {
+        return 4000;
+    }
+
+    public function chunkSize(): int
+    {
+        return 4000;
+    }
+
+}
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,14 +9,70 @@
     <link href="https://cdn.datatables.net/v/bs5/dt-1.13.4/r-2.4.1/datatables.min.css" rel="stylesheet"/>
 </head>
 <body>
-    <nav class="navbar bg-body-tertiary">
+    <nav class="navbar bg-success">
         <div class="container-fluid">
-            <span class="navbar-brand mb-0 h1">Navbar</span>
+            <h1 class="navbar-brand mb-0 h1 text-white">Reporte de Juicio Evaluativo Sede TIC</h1>
         </div>
     </nav>
+
+    <main class="container mt-5">
+        <section class="card border-3 shadow-sm border-success bg-light">
+            <div class="card-body">
+                <h4 class="card-title text-success">Importar Excel de Reportes por Fichas</h4>
+                <form action="" method="post" class="row">
+                    @csrf
+                    <div class="col-md-9 mb-3">
+                        <input class="form-control shadow-sm" type="file" accept=".xlsx" id="fileExcel" name="fileExcel">
+                    </div>
+                    <div class="col-md-3 mb-3">
+                        <button type="submit" class="w-100 btn btn-success fw-semibold shadow-sm">Importar Excel</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+
+        <section class="card border-3 shadow-sm border-success bg-light mt-4">
+            <div class="card-body">
+                <h4 class="card-title text-success">Tabla de Reportes Fichas Sede TIC</h4>
+                <table id="example" class="table table-striped dt-responsive nowrap" style="width:100%">
+                <thead>
+                    <tr>
+                        <th>Ficha y Programa</th>
+                        <th>Tipo de Documento</th>
+                        <th>Nombre Aprendiz</th>
+                        <th>TRIMESTRE I 2020</th>
+                        <th>TRIMESTRE II 2020</th>
+                        <th>TRIMESTRE III 2020</th>
+                        <th>TRIMESTRE IV 2020</th>
+                        <th>TRIMESTRE I 2021</th>
+                        <th>TRIMESTRE II 2021</th>
+                        <th>TRIMESTRE III 2021</th>
+                        <th>TRIMESTRE IV 2021</th>
+                        <th>TRIMESTRE I 2022</th>
+                        <th>TRIMESTRE II 2022</th>
+                        <th>TRIMESTRE III 2022</th>
+                        <th>TRIMESTRE IV 2022</th>
+                        <th>TRIMESTRE I 2023</th>
+                        <th>TRIMESTRE II 2023</th>
+                        <th>Estado</th>
+                        <th>Competencia</th>
+                        <th>Resultado Aprendizaje</th>
+                        <th>Jucio Evaluación</th>
+                        <th>Funcionaro</th>
+                    </tr>
+                </thead>
+                </table>
+            </div>
+        </section>
+    </main>
     
 <script src="{{asset('js/bootstrap.bundle.min.js')}}"></script>
 <script src="{{asset('js/jquery.js')}}"></script>
 <script src="https://cdn.datatables.net/v/bs5/dt-1.13.4/r-2.4.1/datatables.min.js"></script>
+<script>
+    $(document).ready(function() {
+        $('#example').DataTable();
+    } );
+</script>
 </body>
 </html>
